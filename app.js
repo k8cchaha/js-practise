702155d31e6a0d3a67fa0d4f08c576535eb9afb1
Alex_Wang <k8cchaha@gmail.com>
2023-09-12 15:32:37 +0800

    import and export

---
+++ app.js
@@ -0,0 +1,6 @@
+import { mvpPlayer, getBestPlayer } from "./util.js";
+
+
+console.log('Hello App')
+console.log(mvpPlayer)
+console.log(getBestPlayer())
---
+++ util.js
@@ -0,0 +1,4 @@
+export let mvpPlayer = 'kobe bryant'
+export function getBestPlayer() {
+  return 'Michael Jordan'
+}
